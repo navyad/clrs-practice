0b0ecc6687da9651e1148ac3affbe3870e0fdaaa
naveen yadav <navyad.pro@gmail.com>
2016-09-27 17:29:07 +0530

    insertion sort fix

--- insertion_sort.py
+++ insertion_sort.py
@@ -7,15 +7,18 @@ def run_insertion(items):
         element = cards[j]
         previous_index = j-1
         previous_element = cards[previous_index]
-        while previous_index > -1 and previous_element > element:
+        while previous_index >= 0  and previous_element > element:
             cards[previous_index+1] = cards[previous_index]
             previous_index = previous_index-1
+            previous_element = cards[previous_index]
         cards[previous_index+1] = element
     return cards
 
 
 if __name__ == '__main__':
-    cards = [12, 11, 13, 5, 6]
+
+    import random
+    cards = random.sample(xrange(100), 10)
     print "input {0}".format(cards)
     run_insertion(cards)
     print "output {0}".format(cards)
