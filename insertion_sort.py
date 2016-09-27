d2bbc6781b108cb96dfabbdc8ef7edf26e69801c
naveen yadav <navyad.pro@gmail.com>
2016-09-27 18:42:05 +0530

    previous_element reference removed

--- insertion_sort.py
+++ insertion_sort.py
@@ -6,11 +6,9 @@ def run_insertion(items):
     for j in range(1, cards_len):
         element = cards[j]
         previous_index = j-1
-        previous_element = cards[previous_index]
-        while previous_index >= 0  and previous_element > element:
+        while previous_index >= 0 and cards[previous_index] > element:
             cards[previous_index+1] = cards[previous_index]
             previous_index = previous_index-1
-            previous_element = cards[previous_index]
         cards[previous_index+1] = element
     return cards
 
