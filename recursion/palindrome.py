71bfcf27cf906afd74388d34b167991abed4bbad
nav005 <naveen@uninstall.io>
2018-05-07 07:03:17 +0530

    defautl arg

--- recursion/palindrome.py
+++ recursion/palindrome.py
@@ -9,7 +9,6 @@ def iter_palindrome(num):
     reverse_num  = 0
     original_num = num
     while (num!=0):
-        print "........num", num
         remainder = num%10
         reverse_num = reverse_num*10 + remainder
         num/= 10
@@ -23,17 +22,17 @@ iter_palindrome(user_num)
 
 # recursive version
 
-def recur_palindrome(r_reverse, r_num):
+def recur_palindrome(r_num, r_reverse=0):
     if r_num == 0:
         return r_reverse
     else:
         r_reminder = r_num % 10
         r_reverse = r_reverse * 10 + r_reminder
         r_num /= 10
-        return recur_palindrome(r_reverse, r_num)
+        return recur_palindrome(r_num, r_reverse)
 
 
 num = int(raw_input("num: "))
-new_num = recur_palindrome(0, num)
+new_num = recur_palindrome(num)
 is_palin = num == new_num
 print "{0} is palindrome: {1}".format(new_num, is_palin)
